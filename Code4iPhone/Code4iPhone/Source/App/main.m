50afdc954b4165d3232cce51b7aff845ee0005f6
Croath <croathliu@gmail.com>
2013-05-07 20:16:17 +0800

    project created.

---
+++ Code4iPhone/Code4iPhone/Source/App/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Code4iPhone
+//
+//  Created by croath on 13-5-7.
+//  Copyright (c) 2013年 Douban Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
